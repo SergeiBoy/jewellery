4520963a8e13e733db2f12ffbd233dcbc52331a2
SergeiBoy <s_shchetinin@mail.ru>
2020-12-07 01:16:49 +0500

    Сделал десктоп каталога

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var del = require("del");
 var concat = require("gulp-concat");
 
 gulp.task("main-scripts", function() {
-  return gulp.src(["source/js/menu.js", "source/js/slider.js", "source/js/accordion.js"])
+  return gulp.src(["source/js/menu.js", "source/js/slider.js", "source/js/accordion.js", "source/js/accordion-filter.js"])
     .pipe(concat("main.js"))
     .pipe(gulp.dest("build/js"));
 });
---
+++ source/js/accordion-filter.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var accordion = document.querySelector('.shop__filter');
+
+  if (accordion) {
+    var filterItems = accordion.querySelectorAll('.filter__form-item');
+
+    var addingListener = function (item) {
+      var toggle = item.querySelector('button');
+
+      toggle.addEventListener('click', function () {
+        if (item.classList.contains('filter__form-item--opened')) {
+          item.classList.remove('filter__form-item--opened');
+          item.classList.add('filter__form-item--closed');
+        } else {
+          item.classList.remove('filter__form-item--closed');
+          item.classList.add('filter__form-item--opened');
+        }
+      });
+    };
+
+    for (var i = 0; i < filterItems.length; i++) {
+      var item = filterItems[i];
+
+      if (i === 0 || i === 3) {
+        item.classList.add('filter__form-item--opened');
+      } else {
+        item.classList.add('filter__form-item--closed');
+      }
+
+      addingListener(item);
+    }
+  }
+})();
