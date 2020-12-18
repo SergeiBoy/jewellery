9ec9e00fba6b86858fe91458927da360e1d79e27
SergeiBoy <s_shchetinin@mail.ru>
2020-12-18 13:17:35 +0500

    Исправлены замечания после второго тестирования

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,9 @@
   var header = document.querySelector('.page-header');
   var navToggle = document.querySelector('.page-header__toggle');
 
+  var pageMain = document.querySelector('.page-main');
+  var pageFooter = document.querySelector('.page-footer');
+
   header.classList.add('page-header--menu-closed');
 
   navToggle.addEventListener('click', function (evt) {
@@ -12,9 +15,13 @@
     if (header.classList.contains('page-header--menu-closed')) {
       header.classList.remove('page-header--menu-closed');
       header.classList.add('page-header--menu-opened');
+      pageMain.classList.add('page-main--menu-opened');
+      pageFooter.classList.add('page-footer--menu-opened');
     } else {
       header.classList.remove('page-header--menu-opened');
       header.classList.add('page-header--menu-closed');
+      pageMain.classList.remove('page-main--menu-opened');
+      pageFooter.classList.remove('page-footer--menu-opened');
     }
   });
 })();
